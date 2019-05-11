e73d8a9935ff44795e8554f90a1242c265302127
tarotum <onyskiv.maxim@gmail.com>
2019-05-11 18:28:32 +0300

    add rest-and-g layout

--- app/scripts/common.js
+++
@@ -1,9 +0,0 @@
-(function mobileNavHandler() {
-  if (!document.querySelector("#nav-button")) return;
-
-  const navButton = document.querySelector("#nav-button");
-  const nav = document.querySelector(".nav");
-  navButton.onclick = function() {
-    nav.classList.toggle("nav--fullscreen");
-  };
-})();
